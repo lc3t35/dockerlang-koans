a15d426f0812c1872426453ab5bb0912e1890d35
Laurent Roger <laurent.roger@diateam.net>
2015-01-29 11:02:56 +0100

    correct run.sh

--- run.sh
+++ run.sh
@@ -6,15 +6,10 @@ docker build -t lc3t35/dockerlang-koans build
 docker stop dockerlang-koans || true
 docker rm dockerlang-koans || true
 
-ERLANGKOANS=`pwd`
-mkdir -p $ERLANGKOANS/src
-
 docker run \
     -t \
     -i \
     --rm \
     -p 2222:22 \
-    -v $ERLANGKOANS/src:/erlang-koans/src \
-    --hostname dockerlang-koans \
     lc3t35/dockerlang-koans \
     /bin/bash
